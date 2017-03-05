a2f1f4f3ca7dc86cbc6466cfee8539bc6928e38a
danielmoi <daniel@ampersandmoi.com>
2017-03-05 11:33:12 +1100

    try this...

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-import mate from './mate';
-import loudMate from './loudMate';
+// import mate from './mate';
+// import loudMate from './loudMate';
 
-export { mate, loudMate };
+// export { mate, loudMate };
+export mate from './mate';
+// export build, { create } from './builder';
+// export ViewModel from './view_model';
+// export ViewModelCollection from './collection';
