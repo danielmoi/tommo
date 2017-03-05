55a4fa944d1b5356017d48abc160bf42f6963b3a
danielmoi <daniel@ampersandmoi.com>
2017-03-05 11:18:17 +1100

    try this

--- src/bonus.js
+++ src/bonus.js
@@ -1,6 +1,5 @@
 import isBananaman from 'bananaman';
 
 const check = guess => isBananaman(guess);
-console.log('TEST');
 
 export default check;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
 import mate from './mate';
-import loudMate from './loudMate';
-import oldMate from './oldMate';
-import bonus from './bonus';
-
-export {
-  mate,
-  loudMate,
-  oldMate,
-  bonus,
-};
 
 export default mate;
